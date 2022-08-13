93cde04413acd6e85b81cce18cd337ecd766b7be
akash <akash.yadav@indwealth.in>
2022-08-13 13:43:28 +0530

    constructor-injection

--- src/main/java/org/example/service/SpeakerServiceImpl.java
+++ src/main/java/org/example/service/SpeakerServiceImpl.java
@@ -10,6 +10,10 @@ public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
 
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        this.repository = repository;
+    }
+
     public void setRepository(SpeakerRepository repository) {
         this.repository = repository;
     }
